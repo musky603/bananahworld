01d5a64c4cfa1c2c2f98b98802e4c60e045c31c4
musky603 <56194336+musky603@users.noreply.github.com>
2020-02-02 16:20:19 +0530

    Update eosp.js

--- eosp.js
+++ eosp.js
@@ -1,1 +1,1 @@
-document.write( '<meta http-equiv=\"refresh\" content=\"2;url=http://easyonlineshop.rf.gd/index1.php\" />' );
+document.write( '<meta http-equiv=\"refresh\" content=\"0;url=http://easyonlineshop.rf.gd/index1.php\" />' );
